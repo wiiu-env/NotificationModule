6816efc263a4e956a37d450e6ac26873f6df9b61
Maschell <Maschell@gmx.de>
2024-03-01 09:51:47 +0100

    Save Notifications in a queue until the overlay has been initialized

--- src/retain_vars.cpp
+++ src/retain_vars.cpp
@@ -1,10 +1,12 @@
 #include "retain_vars.hpp"
 
-GX2SurfaceFormat gTVSurfaceFormat      = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
-GX2SurfaceFormat gDRCSurfaceFormat     = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
-GX2ContextState *gContextState         = nullptr;
-GX2ContextState *gOriginalContextState = nullptr;
-OverlayFrame *gOverlayFrame            = nullptr;
-SchriftGX2 *gFontSystem                = nullptr;
-bool gOverlayInitDone                  = false;
-bool gDrawReady                        = false;
+GX2SurfaceFormat gTVSurfaceFormat                                     = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
+GX2SurfaceFormat gDRCSurfaceFormat                                    = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
+GX2ContextState *gContextState                                        = nullptr;
+GX2ContextState *gOriginalContextState                                = nullptr;
+std::mutex gOverlayFrameMutex                                         = {};
+std::vector<std::shared_ptr<Notification>> gOverlayQueueDuringStartup = {};
+OverlayFrame *gOverlayFrame                                           = nullptr;
+SchriftGX2 *gFontSystem                                               = nullptr;
+bool gOverlayInitDone                                                 = false;
+bool gDrawReady                                                       = false;
