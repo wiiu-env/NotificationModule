c8dbca8fade0d5c84379cd95f7542d10baa6669d
Daniel K. O. (dkosmari) <none@none>
2024-05-31 17:50:24 -0300

    Use steady/monotonic clock for the timers, so they can handle realtime clock jumps.

--- src/gui/Timer.h
+++ src/gui/Timer.h
@@ -3,15 +3,15 @@
 
 class Timer {
 public:
-    Timer() { clock_gettime(CLOCK_REALTIME, &beg_); }
+    Timer() { clock_gettime(CLOCK_MONOTONIC, &beg_); }
 
     double elapsed() {
-        clock_gettime(CLOCK_REALTIME, &end_);
+        clock_gettime(CLOCK_MONOTONIC, &end_);
         return end_.tv_sec - beg_.tv_sec +
                (end_.tv_nsec - beg_.tv_nsec) / 1000000000.;
     }
 
-    void reset() { clock_gettime(CLOCK_REALTIME, &beg_); }
+    void reset() { clock_gettime(CLOCK_MONOTONIC, &beg_); }
 
 private:
     timespec beg_, end_;
